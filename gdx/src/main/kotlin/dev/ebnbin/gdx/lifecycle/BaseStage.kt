e8b4b5288bbf7bb091e8951583ce72ff24b669b0
Bin Zhang <ebnbin@gmail.com>
2023-06-02 16:54:33 -0400

    Update BaseStage

--- gdx/src/main/kotlin/dev/ebnbin/gdx/lifecycle/BaseStage.kt
+++ gdx/src/main/kotlin/dev/ebnbin/gdx/lifecycle/BaseStage.kt
@@ -1,15 +1,10 @@
 package dev.ebnbin.gdx.lifecycle
 
-import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.Viewport
 import dev.ebnbin.gdx.utils.UnitScreenViewport
 
-abstract class BaseStage : Stage {
-    constructor(viewport: Viewport = UnitScreenViewport()) : super(viewport)
-
-    constructor(viewport: Viewport = UnitScreenViewport(), batch: Batch) : super(viewport, batch)
-
+abstract class BaseStage(viewport: Viewport = UnitScreenViewport()) : Stage(viewport) {
     open fun resize(width: Float, height: Float) {
     }
 
