6b2e90994b9bfcec63518832bc79b18e7d863570
Bin Zhang <ebnbin@gmail.com>
2023-06-02 13:47:04 -0400

    Update DesktopLauncher

--- desktop/src/main/kotlin/dev/ebnbin/insaniquarium/DesktopLauncher.kt
+++ desktop/src/main/kotlin/dev/ebnbin/insaniquarium/DesktopLauncher.kt
@@ -2,12 +2,13 @@ package dev.ebnbin.insaniquarium
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.badlogic.gdx.graphics.glutils.HdpiMode
 
 fun main() {
     val listener = LauncherHelper.createGame()
     val config = Lwjgl3ApplicationConfiguration().also {
-        it.setTitle("Insaniquarium")
-        it.setForegroundFPS(60)
+        it.setMaximized(true)
+        it.setHdpiMode(HdpiMode.Pixels)
     }
     Lwjgl3Application(listener, config)
 }
