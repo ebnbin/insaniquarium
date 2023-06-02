5ca50ab40dcd1dc9af8ed73032bf509e0e1ea78b
Bin Zhang <ebnbin@gmail.com>
2023-06-02 14:43:28 -0400

    Create BaseGame

---
+++ gdx/src/main/kotlin/dev/ebnbin/gdx/BaseGame.kt
@@ -0,0 +1,23 @@
+package dev.ebnbin.gdx
+
+import com.badlogic.gdx.ApplicationListener
+
+abstract class BaseGame : ApplicationListener {
+    override fun create() {
+    }
+
+    override fun resize(width: Int, height: Int) {
+    }
+
+    override fun render() {
+    }
+
+    override fun pause() {
+    }
+
+    override fun resume() {
+    }
+
+    override fun dispose() {
+    }
+}
