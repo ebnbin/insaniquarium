f39d088c40254fbd3f25db8e084b7de9c06d95ab
Bin Zhang <ebnbin@gmail.com>
2023-06-02 12:51:21 -0400

    Update ios module "build.gradle.kts"

---
+++ ios/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm")
+    id("robovm")
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(Dependencies.ROBOVM_RT)
+    implementation(Dependencies.ROBOVM_COCOATOUCH)
+    implementation(Dependencies.GDX_BACKEND_ROBOVM)
+    implementation(Dependencies.GDX_PLATFORM_NATIVES_IOS)
+}
