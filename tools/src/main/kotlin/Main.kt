3a47f22ee23ec6160e440329545112ef0b4fc509
Bin Zhang <ebnbin@gmail.com>
2023-06-02 23:21:22 -0400

    Add aquarium texture assets

--- tools/src/main/kotlin/Main.kt
+++ tools/src/main/kotlin/Main.kt
@@ -5,4 +5,24 @@ fun main() {
         srcFile = File("../files/Insaniquarium Deluxe/images/aquarium1.jpg"),
         dstFile = File("../assets/texture/aquarium_a.png"),
     )
+    ImageHelper.aquarium(
+        srcFile = File("../files/Insaniquarium Deluxe/images/aquarium4.jpg"),
+        dstFile = File("../assets/texture/aquarium_b.png"),
+    )
+    ImageHelper.aquarium(
+        srcFile = File("../files/Insaniquarium Deluxe/images/aquarium3.jpg"),
+        dstFile = File("../assets/texture/aquarium_c.png"),
+    )
+    ImageHelper.aquarium(
+        srcFile = File("../files/Insaniquarium Deluxe/images/aquarium2.jpg"),
+        dstFile = File("../assets/texture/aquarium_d.png"),
+    )
+    ImageHelper.aquarium(
+        srcFile = File("../files/Insaniquarium Deluxe/images/aquarium6.jpg"),
+        dstFile = File("../assets/texture/aquarium_e.png"),
+    )
+    ImageHelper.aquarium(
+        srcFile = File("../files/Insaniquarium Deluxe/images/Aquarium5.jpg"),
+        dstFile = File("../assets/texture/aquarium_f.png"),
+    )
 }
