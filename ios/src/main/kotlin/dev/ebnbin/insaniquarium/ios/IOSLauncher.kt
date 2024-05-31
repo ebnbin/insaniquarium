c0dc1a8dd20389db0baa6303b948b5b329924a76
Bin Zhang <ebnbin@gmail.com>
2024-05-31 23:44:02 +0800

    Update IOSLauncher

--- ios/src/main/kotlin/dev/ebnbin/insaniquarium/ios/IOSLauncher.kt
+++ ios/src/main/kotlin/dev/ebnbin/insaniquarium/ios/IOSLauncher.kt
@@ -1,28 +1,29 @@
-@file:JvmName("IOSLauncher")
-
 package dev.ebnbin.insaniquarium.ios
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration
+import com.badlogic.gdx.graphics.glutils.HdpiMode
 import dev.ebnbin.insaniquarium.LauncherHelper
 import org.robovm.apple.foundation.NSAutoreleasePool
 import org.robovm.apple.uikit.UIApplication
 
-/** Launches the iOS (RoboVM) application. */
 class IOSLauncher : IOSApplication.Delegate() {
     override fun createApplication(): IOSApplication {
-        return IOSApplication(LauncherHelper.createGame(), IOSApplicationConfiguration().apply {
-            // Configure your application here.
-        })
+        val listener = LauncherHelper.createGame()
+        val config = IOSApplicationConfiguration().also {
+            it.hdpiMode = HdpiMode.Pixels
+        }
+        return IOSApplication(listener, config)
     }
 
     companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            val pool = NSAutoreleasePool()
-            val principalClass: Class<UIApplication>? = null
-            val delegateClass = IOSLauncher::class.java
-            UIApplication.main(args, principalClass, delegateClass)
-            pool.close()
+        @JvmStatic
+        fun main(args: Array<String>) {
+            NSAutoreleasePool().use {
+                val principalClass: Class<UIApplication>? = null
+                val delegateClass = IOSLauncher::class.java
+                UIApplication.main(args, principalClass, delegateClass)
+            }
         }
     }
 }
