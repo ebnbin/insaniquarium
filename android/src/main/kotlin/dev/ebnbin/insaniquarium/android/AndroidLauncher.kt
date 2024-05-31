5355da5b1b13b58088de9813b1fb9bce27715e9c
Bin Zhang <ebnbin@gmail.com>
2024-05-31 23:33:04 +0800

    Update AndroidLauncher

--- android/src/main/kotlin/dev/ebnbin/insaniquarium/android/AndroidLauncher.kt
+++ android/src/main/kotlin/dev/ebnbin/insaniquarium/android/AndroidLauncher.kt
@@ -5,13 +5,11 @@ import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import dev.ebnbin.insaniquarium.LauncherHelper
 
-/** Launches the Android application. */
 class AndroidLauncher : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initialize(LauncherHelper.createGame(), AndroidApplicationConfiguration().apply {
-            // Configure your application here.
-            useImmersiveMode = true // Recommended, but not required.
-        })
+        val listener = LauncherHelper.createGame()
+        val config = AndroidApplicationConfiguration()
+        initialize(listener, config)
     }
 }
