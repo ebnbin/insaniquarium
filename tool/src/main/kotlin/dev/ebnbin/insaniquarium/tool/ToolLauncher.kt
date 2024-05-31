eb798b50c4f8e134cfa035d2eccd3efb2df5e02c
Bin Zhang <ebnbin@gmail.com>
2024-05-31 23:56:46 +0800

    Delete HeadlessLauncher; Create ToolLauncher

---
+++ tool/src/main/kotlin/dev/ebnbin/insaniquarium/tool/ToolLauncher.kt
@@ -0,0 +1,39 @@
+@file:JvmName("ToolLauncher")
+
+package dev.ebnbin.insaniquarium.tool
+
+import com.badlogic.gdx.ApplicationListener
+import com.badlogic.gdx.backends.headless.HeadlessApplication
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration
+
+fun main() {
+    runHeadlessApplication {
+    }
+}
+
+private fun runHeadlessApplication(action: () -> Unit) {
+    val listener = object : ApplicationListener {
+        override fun create() {
+            action()
+        }
+
+        override fun resize(width: Int, height: Int) {
+        }
+
+        override fun render() {
+        }
+
+        override fun pause() {
+        }
+
+        override fun resume() {
+        }
+
+        override fun dispose() {
+        }
+    }
+    val config = HeadlessApplicationConfiguration().also {
+        it.updatesPerSecond = -1
+    }
+    HeadlessApplication(listener, config)
+}
