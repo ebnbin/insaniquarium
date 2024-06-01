e8799ad622bb3b7a9ae3d87e8e6a615b0a729c72
Bin Zhang <ebnbin@gmail.com>
2024-06-01 23:40:38 +0800

    Create DevLabel

--- kgdx/src/main/kotlin/dev/ebnbin/kgdx/dev/DevInfoStage.kt
+++ kgdx/src/main/kotlin/dev/ebnbin/kgdx/dev/DevInfoStage.kt
@@ -1,24 +1,27 @@
 package dev.ebnbin.kgdx.dev
 
-import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup
 import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.viewport.FitViewport
 import dev.ebnbin.kgdx.Game
 import dev.ebnbin.kgdx.LifecycleStage
-import dev.ebnbin.kgdx.game
 
 internal class DevInfoStage : LifecycleStage(FitViewport(Game.WORLD_WIDTH, Game.WORLD_HEIGHT)) {
-    private val label: Label = run {
-        val freeTypeAsset = game.assets.freeType("kgdx_noto_sans_mono")
-        Label(null, Label.LabelStyle(freeTypeAsset.get(), null)).also {
-            it.setFillParent(true)
-            it.setAlignment(Align.topRight)
-            addActor(it)
+    init {
+        VerticalGroup().also { verticalGroup ->
+            verticalGroup.setFillParent(true)
+            verticalGroup.align(Align.topRight)
+            verticalGroup.columnAlign(Align.right)
+            val devLabel = DevLabel(
+                entry = DevLabel.Entry(
+                    key = "delta",
+                    getValue = {
+                        "%.3f".format(it)
+                    },
+                ),
+            )
+            verticalGroup.addActor(devLabel)
+            addActor(verticalGroup)
         }
-    }
-
-    override fun act(delta: Float) {
-        super.act(delta)
-        label.setText("$delta")
     }
 }
---
+++ kgdx/src/main/kotlin/dev/ebnbin/kgdx/dev/DevLabel.kt
@@ -0,0 +1,43 @@
+package dev.ebnbin.kgdx.dev
+
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.Pixmap
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import dev.ebnbin.kgdx.game
+import ktx.assets.disposeSafely
+import ktx.graphics.copy
+
+internal class DevLabel(
+    private val entry: Entry,
+) : Label(null, LabelStyle(game.assets.freeType("kgdx_noto_sans_mono").get(), null)) {
+    data class Entry(
+        val key: String,
+        val getValue: (delta: Float) -> String,
+    )
+
+    override fun act(delta: Float) {
+        super.act(delta)
+        val text = " ${entry.key}=${entry.getValue(delta)} "
+        setText(text)
+    }
+
+    private val backgroundTexture: Texture = run {
+        val pixmap = Pixmap(1, 1, Pixmap.Format.RGBA8888)
+        pixmap.drawPixel(0, 0, Color.WHITE.toIntBits())
+        val texture = Texture(pixmap)
+        pixmap.disposeSafely()
+        texture
+    }
+
+    private val backgroundColor: Color = Color.BLACK.copy(alpha = 0.25f)
+
+    override fun draw(batch: Batch, parentAlpha: Float) {
+        val oldColor = batch.color
+        batch.color = backgroundColor
+        batch.draw(backgroundTexture, x, y, width, height)
+        batch.color = oldColor
+        super.draw(batch, parentAlpha)
+    }
+}
