20354013d2694ce406be1c4ef4ac79ec82d404cc
Bin Zhang <ebnbin@gmail.com>
2024-06-01 16:35:51 +0800

    Read image from zip

--- core/src/main/kotlin/dev/ebnbin/insaniquarium/Insaniquarium.kt
+++ core/src/main/kotlin/dev/ebnbin/insaniquarium/Insaniquarium.kt
@@ -1,10 +1,15 @@
 package dev.ebnbin.insaniquarium
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.ScreenUtils
 import dev.ebnbin.kgdx.Game
 import dev.ebnbin.kgdx.game
+import ktx.assets.disposeSafely
+import java.io.ByteArrayOutputStream
+import java.util.zip.ZipInputStream
 
 val insaniquarium: Insaniquarium
     get() = game as Insaniquarium
@@ -16,7 +21,33 @@ class Insaniquarium : Game() {
     override fun create() {
         super.create()
         spriteBatch = SpriteBatch()
-        texture = Texture("libgdx.png")
+        texture = readTextureFromZip("aquarium1.jpg")
+    }
+
+    private fun readTextureFromZip(imageFileName: String): Texture {
+        val zipFileHandle = Gdx.files.internal("Insaniquarium Deluxe.zip")
+        ZipInputStream(zipFileHandle.read()).use { zipInputStream ->
+            var zipEntry = zipInputStream.nextEntry
+            while (zipEntry != null) {
+                if (zipEntry.name == "images/$imageFileName") {
+                    return ByteArrayOutputStream().use { byteArrayOutputStream ->
+                        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+                        var len: Int
+                        while (zipInputStream.read(buffer, 0, buffer.size).also { len = it } != -1) {
+                            byteArrayOutputStream.write(buffer, 0, len)
+                        }
+                        val byteArray = byteArrayOutputStream.toByteArray()
+                        val pixmap = Pixmap(byteArray, 0, byteArray.size)
+                        Texture(pixmap).also {
+                            pixmap.disposeSafely()
+                        }
+                    }
+                }
+                zipInputStream.closeEntry()
+                zipEntry = zipInputStream.nextEntry
+            }
+        }
+        error(Unit)
     }
 
     override fun render() {
