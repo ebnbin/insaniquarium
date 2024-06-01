b9b3e721a87338675994bdf4c5892f8d808ca795
Bin Zhang <ebnbin@gmail.com>
2024-06-01 10:51:38 +0800

    Convert desktop project build.gradle to build.gradle.kts

---
+++ desktop/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm")
+    application
+}
+
+java.toolchain.languageVersion = JavaLanguageVersion.of(Version.JAVA)
+
+sourceSets.main.configure {
+    resources.srcDirs(rootProject.file("assets").path)
+}
+
+application.mainClass = "dev.ebnbin.insaniquarium.desktop.DesktopLauncher"
+
+dependencies {
+    implementation(project(":core"))
+    implementation(Dependency.GDX_BACKEND_LWJGL3)
+    implementation(Dependency.GDX_PLATFORM_NATIVES_DESKTOP)
+    implementation(Dependency.GDX_FREETYPE_PLATFORM_NATIVES_DESKTOP)
+}
+
+tasks.run.configure {
+    if (System.getProperty("os.name").contains("mac", ignoreCase = true)) {
+        jvmArgs("-XstartOnFirstThread")
+    }
+}
+
+// java -XstartOnFirstThread -jar desktop/build/libs/desktop.jar
+tasks.jar.configure {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    manifest.attributes("Main-Class" to application.mainClass.get())
+}
