49a0695b537cae803eabfbf1684dcdd4295cdc1b
Bin Zhang <ebnbin@gmail.com>
2024-06-06 00:20:12 +0800

    Add pets

--- core/src/main/kotlin/dev/ebnbin/insaniquarium/tank/TankGroup.kt
+++ core/src/main/kotlin/dev/ebnbin/insaniquarium/tank/TankGroup.kt
@@ -10,7 +10,7 @@ class TankGroup : Group() {
 
         BodyActor(
             tankGroup = this,
-            type = BodyType.STINKY,
+            type = BodyType.entries.random(),
         ).also {
             addActor(it)
         }
