53ef88a3f217db785d582fb2fd79b1b1833d1d20
Bin Zhang <ebnbin@gmail.com>
2024-06-06 17:21:43 +0800

    Update DevMenuStage viewport

--- kgdx/src/main/kotlin/dev/ebnbin/kgdx/dev/DevMenuStage.kt
+++ kgdx/src/main/kotlin/dev/ebnbin/kgdx/dev/DevMenuStage.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.utils.Align
 import com.kotcrab.vis.ui.VisUI
 import com.kotcrab.vis.ui.widget.MenuBar
+import dev.ebnbin.kgdx.Game
 import dev.ebnbin.kgdx.LifecycleStage
 import dev.ebnbin.kgdx.Screen
 import dev.ebnbin.kgdx.game
@@ -11,21 +12,29 @@ import dev.ebnbin.kgdx.preference.KgdxPreferenceManager
 import dev.ebnbin.kgdx.util.CLEAR_COLOR_MAP
 import dev.ebnbin.kgdx.util.Dpi
 import dev.ebnbin.kgdx.util.TextureFilter
-import dev.ebnbin.kgdx.util.WorldFitViewport
+import dev.ebnbin.kgdx.util.WorldScreenViewport
 import dev.ebnbin.kgdx.util.checkBoxMenuItem
 import dev.ebnbin.kgdx.util.listMenuItem
 import dev.ebnbin.kgdx.util.menuItem
 import ktx.scene2d.vis.menu
 
-internal class DevMenuStage : LifecycleStage(WorldFitViewport()) {
+internal class DevMenuStage : LifecycleStage(WorldScreenViewport()) {
     init {
         VisUI.load(VisUI.SkinScale.X2)
-        MenuBar().also {
-            it.table.setPosition(0f, height)
-            it.table.align(Align.topLeft)
-            it.createDevMenu()
-            addActor(it.table)
-        }
+    }
+
+    private val menuBar: MenuBar = MenuBar().also {
+        it.table.align(Align.topLeft)
+        it.createDevMenu()
+        addActor(it.table)
+    }
+
+    override fun resize(width: Float, height: Float) {
+        super.resize(width, height)
+        menuBar.table.setPosition(
+            (width - Game.WORLD_WIDTH) / 2f,
+            height - (height - Game.WORLD_HEIGHT) / 2f,
+        )
     }
 
     private fun MenuBar.createDevMenu() {
