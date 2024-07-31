feb989941fad70e71c4276a535eb05eeccaa3f57
Bin Zhang <ebnbin@gmail.com>
2024-07-31 00:09:03 -0700

    Import AshleyGame from https://github.com/libgdx/ashley

---
+++ desktop/src/main/java/demo/ashley/AshleyGame.java
@@ -0,0 +1,58 @@
+package demo.ashley;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import demo.ashley.components.MovementComponent;
+import demo.ashley.components.PositionComponent;
+import demo.ashley.components.VisualComponent;
+import demo.ashley.systems.MovementSystem;
+import demo.ashley.systems.RenderSystem;
+
+public class AshleyGame extends ApplicationAdapter {
+    PooledEngine engine;
+
+    @Override
+    public void create() {
+        OrthographicCamera camera = new OrthographicCamera(640, 480);
+        camera.position.set(320, 240, 0);
+        camera.update();
+
+        Texture crateTexture = new Texture("assets/crate.png");
+        Texture coinTexture = new Texture("assets/coin.png");
+
+        engine = new PooledEngine();
+        engine.addSystem(new RenderSystem(camera));
+        engine.addSystem(new MovementSystem());
+
+        Entity crate = engine.createEntity();
+        crate.add(new PositionComponent(50, 50));
+        crate.add(new VisualComponent(new TextureRegion(crateTexture)));
+
+        engine.addEntity(crate);
+
+        TextureRegion coinRegion = new TextureRegion(coinTexture);
+
+        for (int i = 0; i < 100; i++) {
+            Entity coin = engine.createEntity();
+            coin.add(new PositionComponent(MathUtils.random(640), MathUtils.random(480)));
+            coin.add(new MovementComponent(10.0f, 10.0f));
+            coin.add(new VisualComponent(coinRegion));
+            engine.addEntity(coin);
+        }
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        engine.update(Gdx.graphics.getDeltaTime());
+    }
+}
--- desktop/src/main/kotlin/dev/ebnbin/insaniquarium/desktop/DesktopLauncher.kt
+++ desktop/src/main/kotlin/dev/ebnbin/insaniquarium/desktop/DesktopLauncher.kt
@@ -5,12 +5,13 @@ package dev.ebnbin.insaniquarium.desktop
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.badlogic.gdx.graphics.glutils.HdpiMode
-import dev.ebnbin.insaniquarium.LauncherHelper
+import demo.ashley.AshleyGame
 
 fun main() {
-    val listener = LauncherHelper.createGame()
+    val listener = AshleyGame()
     val config = Lwjgl3ApplicationConfiguration().also {
-        it.setMaximized(true)
+//        it.setMaximized(true)
+        it.setWindowedMode(640, 480)
         it.setHdpiMode(HdpiMode.Pixels)
     }
     Lwjgl3Application(listener, config)
